22cd4aceb89cac8f477f447dbdc226e54e5cd384
Kosuke Suzuki <kosuke.suzuki@happyelements.co,jp>
2016-08-23 23:47:34 +0900

    add rate

--- store1_market1.js
+++ store1_market1.js
@@ -5,27 +5,29 @@
       var histories = ObentoMarket.getHistory();
 
       var activity = {};
+      var purchaseNum = activity.purchaseNum;
       const minOniPri = 2000;
       const matsuPri = 40;
       const getMatsuPri = 1200;
       const stromPro = 0.1;
+      const rate = 0.8;
       maxNum = Math.floor((minOniPri - storeInfo.capitalStock)/(stromPro*matsuPri - getMatsuPri))-3;
       if(day == 1)
       {
-        activity.purchaseNum = 300;
+        purchaseNum = 300;
         activity.obentoId = 'TAKE';
       }
       else if(day >= 2){
         var yesterday = histories[day - 2];
         var actual = yesterday.storeActuals[myStore.id];
         if(yesterday.weather == ObentoMarket.Weather.SHINE){
-          activity.purchaseNum = 300;
+          activity.purchaseNum = Math.floor((storeInfo.capitalStock/1200)*rate);
           activity.obentoId = 'MATSU';
         }else if (yesterday.weather == ObentoMarket.Weather.CLOUD){
-          activity.purchaseNum = 300;
+          activity.purchaseNum = Math.floor((storeInfo.capitalStock/1000)*rate);
           activity.obentoId = 'TAKE';
         }else if (yesterday.weather == ObentoMarket.Weather.RAIN){
-          activity.purchaseNum = 300;
+          activity.purchaseNum = Math.floor((storeInfo.capitalStock/800)*rate);
           activity.obentoId = 'UME';
         }
         else
@@ -41,3 +43,9 @@
   );
 
 })();
+
+function SetpurchaseNum(rate, price){
+  var purchaseNum;
+  purchaseNum = Math.floor((capitalStock/price)*rate)
+  return purchaseNum;
+}
